ad1efadf66193b26fb2ad98f246531d0395f2744
iran.mansoori85@gmail.com <iran.mansoori85@gmail.com@df786763-28af-51ab-c9a4-2540f307c378>
2014-11-16 14:30:36 +0000

    Committed first ROS-ized version. --F.

---
+++ trunk/active_perception/src/main/java/se/oru/aass/lucia2014/TestGeometricConstraintSolverSmall.java
@@ -0,0 +1,28 @@
+package se.oru.aass.lucia2014;
+
+import java.util.Vector;
+
+import org.metacsp.framework.ConstraintNetwork;
+import org.metacsp.framework.Variable;
+import org.metacsp.spatial.geometry.GeometricConstraint;
+import org.metacsp.spatial.geometry.GeometricConstraintSolver;
+import org.metacsp.spatial.geometry.Polygon;
+import org.metacsp.spatial.geometry.Vec2;
+import org.metacsp.utility.UI.PolygonFrame;
+
+
+public class TestGeometricConstraintSolverSmall {
+
+	public static void main(String[] args) {
+
+		GeometricConstraintSolver solver = new GeometricConstraintSolver();
+		Variable var = solver.createVariable();
+		
+		Polygon p0 = (Polygon)var;
+		p0.setMovable(true);
+		
+		ConstraintNetwork.draw(solver.getConstraintNetwork());
+		PolygonFrame pf = new PolygonFrame("Polygon Constraint Network", solver.getConstraintNetwork(), 0.1f);
+	}
+
+}
