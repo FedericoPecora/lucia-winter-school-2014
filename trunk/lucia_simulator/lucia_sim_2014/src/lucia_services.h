a006124fd56ff5488924d48da584d43cad2dfbfe
federico.pecora <federico.pecora@df786763-28af-51ab-c9a4-2540f307c378>
2014-12-01 12:07:18 +0000

    Added services for extra panels. --F.

--- trunk/lucia_simulator/lucia_sim_2014/src/lucia_services.h
+++ trunk/lucia_simulator/lucia_sim_2014/src/lucia_services.h
@@ -36,7 +36,9 @@
  double panel1_x1, panel1_x2, panel1_y1, panel1_y2,
         panel2_x1, panel2_x2, panel2_y1, panel2_y2,
         panel3_x1, panel3_x2, panel3_y1, panel3_y2,
-        panel4_x1, panel4_x2, panel4_y1, panel4_y2;
+        panel4_x1, panel4_x2, panel4_y1, panel4_y2,
+        panel5_x1, panel5_x2, panel5_y1, panel5_y2,
+        panel6_x1, panel6_x2, panel6_y1, panel6_y2;
 
 //statusOfMove < 0 <=> not moving
 int statusOfMove = -1;
