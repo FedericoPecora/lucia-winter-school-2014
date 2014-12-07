8bfce3f974b6bdfb26c4711b469b701f20034e24
iran.mansoori85 <iran.mansoori85@gmail.com>
2014-12-07 15:53:04 +0000

    few refactor on solutions

--- lucia_simulator/lucia_meta_csp_lecture/active_perception/src/main/java/se/oru/aass/lucia_meta_csp_lecture/solutions/Part1b.java
+++ lucia_simulator/lucia_meta_csp_lecture/active_perception/src/main/java/se/oru/aass/lucia_meta_csp_lecture/solutions/Part1b.java
@@ -31,24 +31,24 @@ public class Part1b  {
 	
 	public static void main(String[] args) {
 		
-		ActivityNetworkSolver ans = new  ActivityNetworkSolver(0, 100000000);
+		ActivityNetworkSolver activityNetworkSolver = new  ActivityNetworkSolver(0, 100000000);
 		
-		Variable var1 = (SymbolicVariableActivity)ans.createVariable("turtlebot_1");
+		Variable var1 = (SymbolicVariableActivity)activityNetworkSolver.createVariable("turtlebot_1");
 		((SymbolicVariableActivity)var1).setSymbolicDomain("move_base");
 		
-		Variable var2 = (SymbolicVariableActivity)ans.createVariable("turtlebot_2");
+		Variable var2 = (SymbolicVariableActivity)activityNetworkSolver.createVariable("turtlebot_2");
 		((SymbolicVariableActivity)var2).setSymbolicDomain("move_base");
 		
 		//adding constraint
 		AllenIntervalConstraint release = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Release, new Bounds(3000,APSPSolver.INF));
 		release.setFrom(var1);
 		release.setTo(var1);
-		ans.addConstraint(release);
+		activityNetworkSolver.addConstraint(release);
 		
 		AllenIntervalConstraint overlaps = new AllenIntervalConstraint(AllenIntervalConstraint.Type.OverlappedBy, new Bounds(5000,APSPSolver.INF), new Bounds(1,APSPSolver.INF), new Bounds(1,APSPSolver.INF));
 		overlaps.setFrom(var2);
 		overlaps.setTo(var1);
-		ans.addConstraint(overlaps);
+		activityNetworkSolver.addConstraint(overlaps);
 		
 		
 		
@@ -61,7 +61,7 @@ public class Part1b  {
 			}
 		};
 		
-		ConstraintNetworkAnimator animator = new ConstraintNetworkAnimator(ans, 1000, cb);
+		ConstraintNetworkAnimator animator = new ConstraintNetworkAnimator(activityNetworkSolver, 1000, cb);
 		Vector<DispatchingFunction> dispatches = new Vector<DispatchingFunction>();
 		
 		DispatchingFunction robot1DispatchingFunction = new DispatchingFunction("turtlebot_1") {
@@ -92,14 +92,14 @@ public class Part1b  {
 		};
 		dispatches.add(robot2DispatchingFunction);
 		
-		animator.addDispatchingFunctions(ans, dispatches.toArray(new DispatchingFunction[dispatches.size()]));
+		animator.addDispatchingFunctions(activityNetworkSolver, dispatches.toArray(new DispatchingFunction[dispatches.size()]));
 
 		//#################################################################################
 		//visualize
 		//#################################################################################
-		ConstraintNetwork.draw(ans.getConstraintNetwork(),"Activity network");
+		ConstraintNetwork.draw(activityNetworkSolver.getConstraintNetwork(),"Activity network");
 
-		TimelinePublisher tp = new TimelinePublisher((ActivityNetworkSolver)ans, new Bounds(0,120000), true, "turtlebot_1", "turtlebot_2");
+		TimelinePublisher tp = new TimelinePublisher((ActivityNetworkSolver)activityNetworkSolver, new Bounds(0,120000), true, "turtlebot_1", "turtlebot_2");
 		TimelineVisualizer tv = new TimelineVisualizer(tp);
 		tv.startAutomaticUpdate(1000);
 		
