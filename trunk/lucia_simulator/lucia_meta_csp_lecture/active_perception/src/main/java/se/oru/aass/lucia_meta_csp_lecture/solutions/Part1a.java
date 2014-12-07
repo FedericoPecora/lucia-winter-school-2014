a80a03ba4f47c95a00a9b8617735c3d04a21cee9
iran.mansoori85@gmail.com <iran.mansoori85@gmail.com@df786763-28af-51ab-c9a4-2540f307c378>
2014-12-07 15:53:04 +0000

    few refactor on solutions

--- trunk/lucia_simulator/lucia_meta_csp_lecture/active_perception/src/main/java/se/oru/aass/lucia_meta_csp_lecture/solutions/Part1a.java
+++ trunk/lucia_simulator/lucia_meta_csp_lecture/active_perception/src/main/java/se/oru/aass/lucia_meta_csp_lecture/solutions/Part1a.java
@@ -1,29 +1,14 @@
 package se.oru.aass.lucia_meta_csp_lecture.solutions;
 
-import java.util.Vector;
 
-import services.sendGoal;
-import services.sendGoalRequest;
-import services.sendGoalResponse;
-
-import org.metacsp.dispatching.DispatchingFunction;
 import org.metacsp.framework.ConstraintNetwork;
 import org.metacsp.framework.Variable;
 import org.metacsp.multi.activity.ActivityNetworkSolver;
 import org.metacsp.multi.activity.SymbolicVariableActivity;
 import org.metacsp.multi.allenInterval.AllenIntervalConstraint;
-import org.metacsp.sensing.ConstraintNetworkAnimator;
-import org.metacsp.sensing.InferenceCallback;
 import org.metacsp.time.APSPSolver;
 import org.metacsp.time.Bounds;
-import org.metacsp.utility.timelinePlotting.TimelinePublisher;
-import org.metacsp.utility.timelinePlotting.TimelineVisualizer;
-import org.ros.exception.RemoteException;
-import org.ros.exception.RosRuntimeException;
-import org.ros.exception.ServiceNotFoundException;
-import org.ros.node.ConnectedNode;
-import org.ros.node.service.ServiceClient;
-import org.ros.node.service.ServiceResponseListener;
+
 
 public class Part1a  {
 
@@ -31,30 +16,30 @@ public class Part1a  {
 	
 	public static void main(String[] args) {
 		
-		ActivityNetworkSolver ans = new  ActivityNetworkSolver(0, 100000000);
+		ActivityNetworkSolver activityNetworkSolver = new  ActivityNetworkSolver(0, 100000000);
 		
-		Variable var1 = (SymbolicVariableActivity)ans.createVariable("turtlebot_1");
+		Variable var1 = (SymbolicVariableActivity)activityNetworkSolver.createVariable("turtlebot_1");
 		((SymbolicVariableActivity)var1).setSymbolicDomain("move_base");
 		
-		Variable var2 = (SymbolicVariableActivity)ans.createVariable("turtlebot_2");
+		Variable var2 = (SymbolicVariableActivity)activityNetworkSolver.createVariable("turtlebot_2");
 		((SymbolicVariableActivity)var2).setSymbolicDomain("move_base");
 		
-		//adding constraint
+		//adding constraints
 		AllenIntervalConstraint release = new AllenIntervalConstraint(AllenIntervalConstraint.Type.Release, new Bounds(3000,APSPSolver.INF));
 		release.setFrom(var1);
 		release.setTo(var1);
-		ans.addConstraint(release);
+		activityNetworkSolver.addConstraint(release);
 		
 		
 		AllenIntervalConstraint overlaps = new AllenIntervalConstraint(AllenIntervalConstraint.Type.OverlappedBy, new Bounds(5000,APSPSolver.INF), new Bounds(1,APSPSolver.INF), new Bounds(1,APSPSolver.INF));
 		overlaps.setFrom(var2);
 		overlaps.setTo(var1);
-		ans.addConstraint(overlaps);
+		activityNetworkSolver.addConstraint(overlaps);
 		
 		//#################################################################################
 		//visualize
 		//#################################################################################
-		ConstraintNetwork.draw(ans.getConstraintNetwork(),"Activity network");
+		ConstraintNetwork.draw(activityNetworkSolver.getConstraintNetwork(),"Activity network");
 
 
 		
